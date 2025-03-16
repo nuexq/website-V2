0e566f71ff6131a5334c89ad6741a2caf625e491
nuexq <hi.yassir.op@gmail.com>
2025-03-16 12:05:50 +0000

    feat: a site config

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "nuexq",
+  url: "https://nuexq.vercel.app/",
+  description: "my portfolio site",
+  author: "nuexq",
+  links: {
+    github: "https://github.com/nuexq",
+    twitter: "https://x.com/_neuxq",
+    bluesky: "https://bsky.app/profile/nuexq.bsky.social"
+  },
+};
+
+export type SiteConfig = typeof siteConfig;

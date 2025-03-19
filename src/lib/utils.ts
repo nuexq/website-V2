a58e58e92781e239339f782e9b8deaaf1eee66c1
nuexq <hi.yassir.op@gmail.com>
2025-03-19 13:13:25 +0000

    feat(writing): sorted posts by date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,8 +11,35 @@ export function formateDate(input: string | number): string {
   const day = String(date.getDate()).padStart(2, "0");
   const year = date.getFullYear();
 
-  const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(date);
+  const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
+    date,
+  );
 
   return `${month} ${day}, ${year}`;
 }
 
+export interface PostData {
+  title: string;
+  description: string;
+  date: Date;
+}
+
+interface Post {
+  id: string;
+  data: PostData;
+  body: string;
+  filePath: string;
+  digest: string;
+  deferredRender: boolean;
+  collection: string;
+  slug: string;
+  render: Function;
+}
+
+export function sortPosts(posts: Post[]): Post[] {
+  return posts.sort((a, b) => {
+    if (a.data.date > b.data.date) return -1;
+    if (a.data.date < b.data.date) return 1;
+    return 0;
+  });
+}

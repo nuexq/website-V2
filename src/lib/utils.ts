b2ecc5d319840498ec203067882277f60c908ce1
nuexq <hi.yassir.op@gmail.com>
2025-05-08 19:42:21 +0100

    p

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,4 @@
 import type { CollectionEntry } from "astro:content";
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
 
 export function formateDate(input: Date): string {
   const date = new Date(input);

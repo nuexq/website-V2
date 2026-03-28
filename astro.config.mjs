1daa31f03d244e65b0ba5cae786345c3f3eb20da
nuexq <nuexqq@gmail.com>
2026-03-28 19:29:39 +0100

    fix(icons): broken icon package, replaced with astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import {
   transformerNotationDiff,
@@ -70,5 +71,6 @@ export default defineConfig({
       },
       render: fnRender,
     }),
+    icon(),
   ],
 });
